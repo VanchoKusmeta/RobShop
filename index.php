a1820ba8103bb509a2a04ed8864a69f336ca68a9
root <root@srv03189.soton.ac.uk>
2021-12-10 15:25:55 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<h2>Hello World!</h2>
+
+<p>My team number is: <em>0</em></p>
+<p>My team name is: <em>Blank</em></p>
+
+<p>In my team there are:</p>
+<ul>
+<li>Nobody</li>
+</ul>
---
+++ setup.php
@@ -0,0 +1,20 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+echo '<p>Fixing permissions</p>';
+`chmod 0755 ./`;
+`chmod -R u+rwx ./`;
+`find ./ -type d -exec chmod 0755 {} +`;
+`find ./ -type f -exec chmod 0644 {} +`;
+
+echo '<p>Permissions fixed</p>';
+
+if(!file_exists('config/db.cfg')) {
+		echo '<p>It appears you have lost your database configuration file and password. Please contact Oli</p>';
+}
+
+?>
+
