1cfc7d77641146a385d8bd78c9b2f1b7da1d7650
vv1u19 <vv1u19@soton.ac.uk>
2021-12-19 07:40:49 -0500

    new check comment

--- date.php
+++ date.php
@@ -7,6 +7,7 @@
 			date_default_timezone_set('UTC');
 			echo "Hello! Today is " . date("l jS \of F Y");
 			//Hello! Today is Sunday the 18th of October 2020
+			//lets goooooo hope it works now
 		?>
 	</body>
 </html>
